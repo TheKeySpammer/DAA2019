a94a7d1748747195c8a365ca604539f69be0c2b3
Aamir <gt_ams@yahoo.in>
2019-09-28 01:26:59 +0530

    add prims

--- lab6/Graph.java
+++ lab6/Graph.java
@@ -8,26 +8,43 @@ import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 import java.util.TreeSet;
+
+
 import java.io.*;
 
 public class Graph {
 
-    private static final boolean DEBUG = false;
+    private static final boolean DEBUG = true;
 
     TreeMap<Integer, LinkedList<Edge>> graph;
-    
+
     Graph() {
-        graph = new TreeMap<>();
+        this.graph = new TreeMap<>();
     }
 
-    static class Edge {
+    Graph(Graph gh) {
+        this.graph = new TreeMap<>();
+        for (Map.Entry<Integer, LinkedList<Edge>> entry : gh.graph.entrySet()) {
+            LinkedList<Edge> ll = entry.getValue();
+            LinkedList<Edge> copy = new LinkedList<>();
+            for (Edge edge : ll) {
+                copy.add(new Edge(edge));
+            }
+            this.graph.put((int)entry.getKey(), copy);
+        }
+    }
+
+    static class Edge implements Comparable<Edge> {
         private int from;
         private int to;
         private int weight;
+        private boolean bi;
+
         Edge(int to) {
             this.to = to;
             this.from = 0;
             this.weight = 0;
+            this.bi = true;
         }
 
         Edge(int to, int from) {
@@ -40,10 +57,67 @@ public class Graph {
             this.weight = weight;
         }
 
+        Edge(int to, int from, int weight, boolean bi) {
+            this(to, from ,weight);
+            this.bi = bi;
+        }
+
+        Edge (Edge edge) {
+            this.to = edge.to;
+            this.weight = edge.weight;
+            this.from = edge.from;
+            this.bi = edge.bi;
+        } 
+
+        @Override
+        public int compareTo(Edge edge) {
+            if (this.weight != edge.weight) {
+                return this.weight - edge.weight;
+            } else {
+                // Check if same direction edge are same
+                if (this.from == edge.from && this.to == edge.to) {
+                    return 0;
+                }
+                // Ceck if reverse direction edge are same
+                if (this.bi && this.from == edge.to && this.to == edge.from) {
+                    return 0;
+                }
+                return 1;
+            }
+        }
+
         public Edge getReverse() {
             Edge rev = new Edge(this.from, this.to, this.weight);
             return rev;
         }
+
+        public boolean isParallel(Edge other) {
+            return (this.from == other.from && this.to == other.to);
+        }
+
+        public boolean isLoop() {
+            return this.from == this.to;
+        }
+
+        public static Comparator<Edge> getComparator() {
+            return new Comparator<Edge>() {
+                @Override
+                public int compare(Edge edge1, Edge edge2) {
+                    if (edge1.weight != edge2.weight) {
+                        return edge1.weight - edge2.weight;
+                    } else {
+                        if (edge1.from == edge2.to && edge1.to == edge2.from) {
+                            return 0;
+                        }
+                        if ((edge1.bi || edge2.bi) && edge1.from == edge2.to && edge1.to == edge2.from) {
+                            return 0;
+                        }
+                        return 1;
+                    }
+                }
+            };
+        }
+
 
         public int getFrom() {
             return from;
@@ -71,17 +145,17 @@ public class Graph {
 
         @Override
         public String toString() {
-            return this.from+" --"+this.weight+"--> "+this.to;
+            return this.from + " --" + this.weight + "--> " + this.to;
         }
     }
 
-    public static Graph fromFile(String filename)throws IOException {
+    public static Graph fromFile(String filename) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
         Graph gh = new Graph();
-        while(true) {
+        while (true) {
             String input = br.readLine();
             if (input == null) break;
-            StringTokenizer st =new StringTokenizer(input);
+            StringTokenizer st = new StringTokenizer(input);
             int root = Integer.parseInt(st.nextToken());
             LinkedList<Edge> ll = new LinkedList<>();
             while (st.hasMoreTokens()) {
@@ -95,11 +169,34 @@ public class Graph {
         return gh;
     }
 
+    public Graph makePureGraph() {
+        Graph gh = new Graph(this);
+        for (Map.Entry<Integer, LinkedList<Edge>> entry : gh.graph.entrySet()) {
+            LinkedList<Edge> edges = entry.getValue();
+            LinkedList<Edge> toRemove = new LinkedList<>();
+            for (int i = 0; i < edges.size(); i++) {
+                if (edges.get(i).isLoop()) {
+                    toRemove.add(edges.get(i));
+                    continue;
+                }
+                Edge edgeA = edges.get(i);
+                for (int j = i + 1; j < edges.size(); j++) {
+                    Edge edgeB = edges.get(j);
+                    if (edgeA.isParallel(edgeB)) {
+                        toRemove.add(edges.get(edgeA.weight > edgeB.weight ? i : j));
+                    }
+                }
+            }
+            edges.removeAll(toRemove);
+        }
+        return gh;
+    }
+
     public void printGraph() {
-        for (Map.Entry<Integer, LinkedList<Edge>> entry : graph.entrySet()){
-            System.out.print(entry.getKey()+": ");
+        for (Map.Entry<Integer, LinkedList<Edge>> entry : graph.entrySet()) {
+            System.out.print(entry.getKey() + ": ");
             for (Edge edge : entry.getValue()) {
-                System.out.print("("+edge.getTo()+", "+edge.getWeight()+"), ");
+                System.out.print("(" + edge.getTo() + ", " + edge.getWeight() + "), ");
             }
             System.out.println();
         }
@@ -112,13 +209,13 @@ public class Graph {
         return path;
     }
 
-    private void DFS(int parent, ArrayList<Integer> path, boolean[] visited)  {
+    private void DFS(int parent, ArrayList<Integer> path, boolean[] visited) {
         path.add(parent);
-        visited[parent-1] = true;
+        visited[parent - 1] = true;
         if (this.graph.containsKey(parent)) {
             LinkedList<Edge> edges = this.graph.get(parent);
             for (Edge edge : edges) {
-                if (!visited[edge.to-1]){
+                if (!visited[edge.to - 1]) {
                     DFS(edge.to, path, visited);
                 }
             }
@@ -126,28 +223,21 @@ public class Graph {
     }
 
     public Graph kruskalMST() {
+
+        if (DEBUG) {
+            System.out.println();
+            System.out.println("-------------DEBUG LOG (KURSKAL) START-----------------");
+        }
         Graph gh = new Graph();
         for (int key : this.graph.keySet()) {
             gh.graph.put(key, new LinkedList<Edge>());
         }
 
-        TreeSet<Edge> ts = new TreeSet<>(new Comparator<Edge>() {
-            @Override
-            public int compare(Edge edge1, Edge edge2) {
-                if (edge1.weight != edge2.weight) {
-                    return edge1.weight - edge2.weight;
-                }else {
-                    if (edge1.from == edge2.to && edge1.to == edge2.from) {
-                        return 0;
-                    }
-                    return 1;
-                }
-            }
-        });
+        TreeSet<Edge> ts = new TreeSet<>(Edge.getComparator());
 
         for (Map.Entry<Integer, LinkedList<Edge>> entry : this.graph.entrySet()) {
             for (Edge edge : entry.getValue()) {
-                ts.add(edge); 
+                ts.add(new Edge(edge));
             }
         }
 
@@ -167,22 +257,22 @@ public class Graph {
         for (Edge edge : ts) {
             if (edgeCount == vertexCount - 1) break;
             try {
-                
+
                 uf.union(edge.from - padding, edge.to - padding);
-                gh.graph.get(edge.getFrom()).add(edge);
+                gh.graph.get(edge.getFrom()).add(new Edge(edge));
                 // Bidirection support
                 Edge revEdge = edge.getReverse();
-                gh.graph.get(revEdge.getFrom()).add(revEdge);
+                gh.graph.get(revEdge.getFrom()).add(new Edge(revEdge));
                 edgeCount++;
                 if (DEBUG) {
-                    System.out.println("Adding edge "+edge);
-                    System.out.println("Current Edge Count "+edgeCount);
-                    System.out.println("Union Find set: "+Arrays.toString(uf.parent));
+                    System.out.println("Adding edge " + edge);
+                    System.out.println("Current Edge Count " + edgeCount);
+                    System.out.println("Union Find set: " + Arrays.toString(uf.parent));
                 }
-            }catch(Exception ex) {
+            } catch (Exception ex) {
                 // Skip edge that causes cycle
                 if (DEBUG) {
-                    System.out.println("Edge: "+edge+" makes a cycle");
+                    System.out.println("Edge: " + edge + " makes a cycle");
                 }
                 continue;
             }
@@ -192,8 +282,96 @@ public class Graph {
             System.out.println("Sorted edges order: ");
             System.out.println(ts);
         }
+
+
+        if (DEBUG) {
+            System.out.println("-------------DEBUG LOG (KURSKAL) END-----------------");
+            System.out.println();
+        }
         return gh;
     }
+
+    public Graph primsMst() {
+
+        if (DEBUG) {
+            System.out.println();
+            System.out.println("-------------DEBUG LOG (PRIMS) START-----------------");
+        }
+        Graph gh = new Graph();
+//        Remove all the loops and parellel edges
+        Graph pg = this.makePureGraph();
+//      Keep a set of Standing Edges
+        TreeSet<Edge> stnd = new TreeSet<>(Edge.getComparator());
+        int numberOfnodes = pg.graph.size();
+        if (DEBUG) {
+            System.out.println("Number of nodes: "+numberOfnodes);
+        }
+        // Initialy put first vertex
+        gh.graph.put(pg.graph.firstKey(), new LinkedList<Edge>());
+        for (Edge edge : pg.graph.firstEntry().getValue()) {
+            stnd.add(edge);
+        }
+        
+        if (DEBUG)
+            System.out.println("Initially Standing edges: "+stnd);
+        
+        while (stnd.size() > 0 && gh.graph.size() < numberOfnodes) {
+            // While removing is possible and loops are formed
+            Edge smallest = stnd.first();
+            Edge copyOfSmall = new Edge(smallest);
+            stnd.removeIf(e -> e.compareTo(copyOfSmall) == 0);
+            
+            if (DEBUG)
+                System.out.println("Current smallest Edge: "+smallest);
+
+            while (stnd.size() > 0 && gh.graph.containsKey(smallest.to)) {                
+                if (DEBUG)
+                    System.out.println("Since cycle is formed finding new smallest");
+                smallest = stnd.first();
+                Edge copyOfSmall2 = new Edge(smallest);
+                stnd.removeIf(e -> e.compareTo(copyOfSmall2) == 0);
+            
+                if (DEBUG)
+                    System.out.println("Current Smallest Edge: "+smallest);
+            }
+            // Include the smallest edge in new graph
+            gh.graph.get(smallest.from).add(new Edge(smallest));
+            LinkedList<Edge> temp = new LinkedList<>();
+            temp.add(new Edge(smallest.getReverse()));
+            gh.graph.put(smallest.to, temp);
+            if (DEBUG) {
+                System.out.println("Current Graph: ");
+                gh.printGraph();
+                System.out.println();
+            }
+
+            // Remove the smallest edge from the original graph
+            Edge copyOFSmall = new Edge(smallest);
+            pg.graph.get(smallest.from).removeIf(e -> e.compareTo(copyOFSmall) == 0);
+            pg.graph.get(smallest.to).removeIf(e -> e.compareTo(copyOFSmall) == 0);
+            
+            if (DEBUG) {
+                System.out.println("Original Graph: ");
+                pg.printGraph();
+                System.out.println();
+            }
+
+            // Update standing with edges of the new node
+            for (Edge edge : pg.graph.get(smallest.to)) {
+                stnd.add(edge);
+            }
+            if (DEBUG) {
+                System.out.println("Current Standing: "+stnd);
+            }
+        }
+
+        if (DEBUG) {
+            System.out.println("-------------DEBUG LOG (PRIMS) END-----------------");
+            System.out.println();
+        }
+        return gh;
+    }
+
 
     private static Edge processEdge(String rawEdge) {
         int to = 0, weight = 0;
@@ -236,25 +414,24 @@ public class Graph {
             return findWithoutCache(i) == findWithoutCache(j);
         }
 
-        void union (int i, int j) throws ExceptionCycleCheck {
+        void union(int i, int j) throws ExceptionCycleCheck {
             if (checkIfsameParent(i, j)) throw new ExceptionCycleCheck();
             int xroot = find(i), yroot = find(j);
             if (xroot == yroot) return;
 
             if (this.rank[xroot] < this.rank[yroot]) {
                 this.parent[xroot] = yroot;
-            }
-            else if (this.rank[xroot] > this.rank[yroot]) {
+            } else if (this.rank[xroot] > this.rank[yroot]) {
                 this.parent[yroot] = xroot;
-            }
-            else {
+            } else {
                 this.parent[yroot] = xroot;
                 this.rank[xroot]++;
             }
         }
 
-        class ExceptionCycleCheck extends Exception{
+        class ExceptionCycleCheck extends Exception {
             private static final long serialVersionUID = 1L;
+
             ExceptionCycleCheck() {
                 super("Cycle deteceted");
             }
--- lab6/Runner.java
+++ lab6/Runner.java
@@ -1,17 +1,23 @@
 package lab6;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 class Main {
     public static void main(String[] args)throws IOException {
         Graph gh = Graph.fromFile("./lab6/graphData");
         gh.printGraph();
-        ArrayList<Integer> path = gh.DFS(1);
-        System.out.println("DFS from 1: ");
-        System.out.println(path);
-        Graph mst = gh.kruskalMST();
+        
+        // ArrayList<Integer> path = gh.DFS(1);
+        // System.out.println("DFS from 1: ");
+        // System.out.println(path);
+        Graph kruskal = gh.kruskalMST();
         System.out.println("Minimum spanning tree: ");
-        mst.printGraph();
+        System.out.println("Kruskals: ");
+        kruskal.printGraph();
+        System.out.println("Prims: ");
+        Graph prims= gh.primsMst();
+        prims.printGraph();
+
     }
 }
